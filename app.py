89afef15f2256b5098f7dcc9e2c7d004d3c2c9f8
Quent1-G <qgrizard@gmail.com>
2024-11-27 15:32:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import plotly.express as px
+import streamlit as st
+
+# Titre de l'application
+st.title("Analyse des produits alimentaires")
 
 df = pd.read_csv('nouveau_fichier2.csv', nrows=150000, low_memory=False, sep='\t')
-
-df
 # Liste des colonnes à conserver, incluant le nom et le numéro du produit
 columns_to_keep = [
     'product_name',  # Ajout du nom et du numéro du produit
     'origins', 'origins_tags', 'origins_fr', 'countries_fr', 'main_category_fr'
 ]
-# Demander à l'utilisateur d'entrer un terme de recherche
-search_term = input("Entrez un terme à rechercher dans les noms de produits : ").lower()
+# Zone de saisie pour l'utilisateur
+search_term = st.text_input("Entrez les noms de produits que vous recherchez dans votre panier :").lower()
+
 
 # Filtrer les lignes où 'origins' contient une information non vide
 df_filtered = df[df['origins'].notnull()]
@@ -24,8 +27,6 @@ df_filtered = df_filtered[~df_filtered['product_name'].str.contains('es:Mondo', 
 # Sélectionner les colonnes spécifiées
 df_filtered = df_filtered[columns_to_keep]
 
-# Afficher le DataFrame filtré
-df_filtered
 # Fonction de modification de la colonne "origins_tags"
 def modify_origins_tags(value):
     if isinstance(value, str):
@@ -48,8 +49,6 @@ def modify_origins_tags(value):
 # Appliquer la fonction sur la colonne "origins_tags"
 df_filtered['origins_tags'] = df_filtered['origins_tags'].apply(modify_origins_tags)
 
-# Afficher le DataFrame modifié
-df_filtered
 # This line was missing, leading to the error.
 # Assuming you want to process the 'origins_tags' column of the DataFrame,
 # replace 'df_filtered' and 'origins_tags' with your actual DataFrame and column name.
@@ -67,9 +66,9 @@ for char in origins_tags_string:
 if current_origin:  # Ajoute le dernier élément si la chaîne ne se termine pas par un espace ou une nouvelle ligne
     origins_list.append(current_origin)
 
-origins_list
+
 origins_list = list(set(origins_list))
-origins_list
+
 # Convert origins_list to a DataFrame with a 'Pays' column
 df_origins = pd.DataFrame({"Pays": origins_list}) 
 
@@ -82,5 +81,6 @@ fig = px.choropleth(
     title="Carte des pays uniques"
 )
 
-fig.show()
+    # Afficher la carte dans Streamlit
+    st.plotly_chart(fig)
 
